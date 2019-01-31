39b3b3a7177bf7ce4f948057777b26f93c26caee
Ronnicko Jayson E. Calimlim <34341746+jcalimlim14@users.noreply.github.com>
2019-02-01 03:09:54 +0800

    Coding for Virtual Buttons

---
+++ Signovoca/Assets/vbButtonScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class vbButtonScript : MonoBehaviour, IVirtualButtonEventHandler {
+
+	private GameObject vbButtonsObject;
+
+	// Use this for initialization
+	void Start () {
+
+		vbButtonsObject = GameObject.Find ("cmdAnswer1");
+		vbButtonsObject.GetComponent<VirtualButtonBehaviour> ().RegisterEventHandler (this);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnButtonPressed(VirtualButtonAbstractBehaviour vb){
+	
+		Debug.Log ("BUTTON DOWN!!!");
+	
+	}
+
+	public void OnButtonReleased(VirtualButtonAbstractBehaviour vb){
+
+
+
+	}
+}
