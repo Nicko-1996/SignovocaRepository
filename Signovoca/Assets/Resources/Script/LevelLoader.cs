6466c2a47cc5dca8fac86fb979cc7d447a0a7f7a
jovylouverzano <jovylouverzano@gmail.com>
2019-02-11 01:51:09 +0800

    Hexoganl shape UI
    Hexagonal Gradient UI
    Loading Bar

--- Signovoca/Assets/Resources/Script/LevelLoader.cs
+++ Signovoca/Assets/Resources/Script/LevelLoader.cs
@@ -6,32 +6,32 @@ using UnityEngine.UI;
 
 public class LevelLoader : MonoBehaviour {
 
-	public GameObject loadingScreen;
-	public Slider slider; 
-	public Text progressText;
-	
+    public GameObject loadingScreen;
+    public Slider slider;
+    public Text progressText;
 
-	public void LoadLevel (int sceneIndex)
-	{
-		StartCoroutine(LoadAsynchronously(sceneIndex));
-		
-	}
 
-	IEnumerator LoadAsynchronously (int sceneIndex)
-	{
-		AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+    public void LoadLevel(int sceneIndex)
+    {
+        StartCoroutine(LoadAsynchronously(sceneIndex));
 
-		loadingScreen.SetActive(true);
+    }
 
-		while(!operation.isDone)
-		{
-			float progress = Mathf.Clamp01(operation.progress / .9f);
+    IEnumerator LoadAsynchronously(int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
 
-			slider.value = progress;
-			progressText.text = progress * 100f + "%";
+        loadingScreen.SetActive(true);
 
-			yield return null;
-		}
-	}
-	
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+
+            slider.value = progress;
+            progressText.text = progress * 100f + "%";
+
+            yield return null;
+        }
+    }
+
 }
