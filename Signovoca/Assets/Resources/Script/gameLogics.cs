aa8268f2654355ab5a54970165dd06499d7b17b1
Ronnicko Jayson E. Calimlim <34341746+jcalimlim14@users.noreply.github.com>
2019-02-12 12:09:04 +0800

    Revert(Medyo may error)
    NAWALA LAHAT NG GAWA KO.

--- Signovoca/Assets/Resources/Script/gameLogics.cs
+++ Signovoca/Assets/Resources/Script/gameLogics.cs
@@ -70,9 +70,9 @@ public class gameLogics : MonoBehaviour {
 			X.transform.parent = GameObject.Find ("ImageTarget").transform;
 			X.name = "x" + i;
 			Vector3 pos = X.transform.position;    
-			X.transform.position = new Vector3 (pos.x, pos.y, pos.z);
+			X.transform.position = new Vector3 (pos.x, pos.y + 5f, pos.z + 10f);
 			X.transform.rotation = Quaternion.Euler (0, 180, 0);
-			X.transform.localScale = new Vector3 (0.4f, 1f, 0.4f);
+			X.transform.localScale = new Vector3 (0.4f, 0.4f, 0.4f);
 			if (AnswerNumber == usedValues [i]) {
 				GameAnswer = usedValues2 [i];
 			}
